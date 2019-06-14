ba2524b6b09c8ebcf8fb4daeacc8a20f04de1118
infinitybrad <seoih415@gmail.com>
2019-06-14 20:31:58 +0900

    first req

--- sever.js
+++ sever.js
@@ -3,8 +3,16 @@ const express = require('express');
 const app  =  express();
 const PORT =3000;
 
+app.use('/', (req,res) => {
+    res.status(200).json({
+        message:'It works'
+    });
+});
+
 
 const server = http.createServer(app);
+
+
 
 server.listen(PORT, console.log(`sever started...`));
 
