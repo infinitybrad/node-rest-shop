f5f6a9d40ea0cd78eaf24dba683923f6fa836be2
infinitybrad <seoih415@gmail.com>
2019-06-14 20:19:53 +0900

    first node server

---
+++ sever.js
@@ -0,0 +1,10 @@
+const http = require('http');
+const express = require('express');
+const app  =  express();
+const PORT =3000;
+
+
+const server = http.createServer(app);
+
+server.listen(PORT, console.log(`sever started...`));
+
