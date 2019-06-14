c9ef450040214ba6d99665a474a4c765d09fddd8
infinitybrad <seoih415@gmail.com>
2019-06-14 21:47:34 +0900

    add mongoose

--- sever.js
+++ sever.js
@@ -3,10 +3,19 @@ const express = require('express');
 const app  =  express();
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
+const mongoose =require('mongoose');
 
 
 const productRoutes = require('./api/routes/products');
 const ordersRoutes = require('./api/routes/orders');
+
+const db = 'mongodb+srv://teddy:1234@cluster0-4ahzv.mongodb.net/test?retryWrites=true&w=majority';
+
+
+
+mongoose.connect(db, {useNewUrlParser:true})
+    .then(() => console.log("MongoDB Connected..."))
+    .catch(err => console.log(err));
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:false}));
